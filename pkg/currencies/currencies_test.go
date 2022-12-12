5ba1ff1f8b3888e9cedf0e9f7bd687e40b69a9e2
matm <mathias@monnerville.com>
2022-12-12 21:24:09 +0100

    Add some UT
    Fixes #12

--- pkg/currencies/currencies_test.go
+++ pkg/currencies/currencies_test.go
@@ -75,3 +75,46 @@ func TestAll(t *testing.T) {
 		})
 	}
 }
+
+func TestSelected(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		name  string
+		init  func(*mocks.HTTPClient)
+		after func([]string, error)
+	}{
+		{"route name",
+			func(c *mocks.HTTPClient) {
+				resp := newResponseOK(`{"selectedCurrencies":["xmr","xno"]}`)
+				c.EXPECT().Do(mock.Anything).Run(func(req *http.Request) {
+					assert.Equal("/v1/merchant/coins", req.URL.Path, "bad endpoint")
+				}).Return(resp, nil)
+			}, func(c []string, err error) {
+				assert.NoError(err)
+				assert.EqualValues([]string{"xmr", "xno"}, c)
+			},
+		},
+		{"api error",
+			func(c *mocks.HTTPClient) {
+				c.EXPECT().Do(mock.Anything).Return(nil, errors.New("network error"))
+			}, func(c []string, err error) {
+				assert.Nil(c)
+				require.Error(t, err)
+				assert.Equal("selected-currencies: network error", err.Error())
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := mocks.NewHTTPClient(t)
+			core.UseClient(c)
+			if tt.init != nil {
+				tt.init(c)
+			}
+			got, err := Selected()
+			if tt.after != nil {
+				tt.after(got, err)
+			}
+		})
+	}
+}
