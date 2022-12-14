47e0fb00b3b5be03cc8c2012b3f281ea1538c302
matm <mathias@monnerville.com>
2022-12-14 15:18:08 +0100

    Implement create payment from invoice
    Fixes #15

--- pkg/payments/payment_test.go
+++ pkg/payments/payment_test.go
@@ -74,3 +74,67 @@ func TestNew(t *testing.T) {
 		})
 	}
 }
+
+func TestNewFromInvoice(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		name  string
+		ipa   *InvoicePaymentArgs
+		init  func(*mocks.HTTPClient)
+		after func(*Payment, error)
+	}{
+		{"route check", &InvoicePaymentArgs{},
+			func(c *mocks.HTTPClient) {
+				resp := newResponseOK(`{"payment_id":"1234"}`)
+				c.EXPECT().Do(mock.Anything).Run(func(r *http.Request) {
+					assert.Equal("/v1/invoice-payment", r.URL.Path, "bad endpoint")
+				}).Return(resp, nil)
+			}, func(p *Payment, err error) {
+				assert.NoError(err)
+				assert.NotNil(p)
+				assert.Equal("1234", p.ID)
+			},
+		},
+		{"valid args", &InvoicePaymentArgs{
+			InvoiceID:  "55",
+			PurchaseID: "1234",
+		},
+			func(c *mocks.HTTPClient) {
+				resp := newResponseOK(`{"payment_id":"1234"}`)
+				c.EXPECT().Do(mock.Anything).Return(resp, nil)
+			}, func(p *Payment, err error) {
+				assert.NoError(err)
+				assert.NotNil(p)
+				assert.Equal("1234", p.ID)
+			},
+		},
+		{"nil args", nil, nil,
+			func(p *Payment, err error) {
+				assert.Nil(p)
+				assert.Error(err)
+			},
+		},
+		{"api error", &InvoicePaymentArgs{InvoiceID: "1234"},
+			func(c *mocks.HTTPClient) {
+				c.EXPECT().Do(mock.Anything).Return(nil, errors.New("network error"))
+			}, func(p *Payment, err error) {
+				assert.Nil(p)
+				assert.Error(err)
+				assert.Equal("invoice-payment: network error", err.Error())
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := mocks.NewHTTPClient(t)
+			core.UseClient(c)
+			if tt.init != nil {
+				tt.init(c)
+			}
+			got, err := NewFromInvoice(tt.ipa)
+			if tt.after != nil {
+				tt.after(got, err)
+			}
+		})
+	}
+}
